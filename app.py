1d8ce4a69f46621d44f1e36770de06a7fda78e50
charle <carlospabon2018@gmail.com>
2026-05-14 17:57:10 -0500

    app.py

--- app.py
+++ app.py
@@ -4,13 +4,10 @@ app = Flask(__name__)
 
 @app.route('/saludo', methods=['GET', 'POST'])
 def saludo():
-
     if request.method == 'POST':
-
         nombre = request.form['nombre']
-
-        return f"Hola {nombre}"
-
+        correo = request.form['correo'] # Capturamos el nuevo campo
+        return f"Gracias {nombre}, hemos registrado tu correo: {correo}"
     return render_template('saludo.html')
 
 if __name__ == '__main__':
